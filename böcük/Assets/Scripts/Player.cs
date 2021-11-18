828d37d3920fdccac0d6cc91272a5183db4322ab
DESKTOP-G10MKTB\Deniz <kocak17@itu.edu.tr>
2021-11-18 15:40:57 +0300

    unity project commited

---
+++ böcük/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+
+    public float Speed = 5f;
+
+    public Rigidbody2D rb;
+
+    private Vector3 moveDelta; 
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Reset moveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction, wether you're going right or left
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Move
+        transform.Translate(moveDelta * Time.deltaTime * Speed);
+
+    }
+}
