a9e030dcb25b5bac18968c15182aa27fb8a2c30f
DESKTOP-G10MKTB\Deniz <kocak17@itu.edu.tr>
2021-11-22 21:29:46 +0300

    gün sonu babuş

--- böcük/Assets/Scripts/Player.cs
+++ böcük/Assets/Scripts/Player.cs
@@ -9,7 +9,8 @@ public class Player : MonoBehaviour
     public float Speed = 5f;
 
     public Rigidbody2D rb;
-
+    public Animator animator;
+    private RaycastHit2D hit;
     private Vector3 moveDelta; 
 
     private void Start()
@@ -22,11 +23,26 @@ public class Player : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
+        animator.SetFloat("Horizontal", x);
+        animator.SetFloat("Vertical", y);
+        animator.SetFloat("Speed", moveDelta.sqrMagnitude);
+
+
         // Reset moveDelta
         moveDelta = new Vector3(x, y, 0);
 
-        // Move
-        transform.Translate(moveDelta * Time.deltaTime * Speed);
-
+        //bu ne mk
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Bug", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Move
+            transform.Translate(0, moveDelta.y * Time.deltaTime * Speed, 0);
+        }
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Bug", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Move
+            transform.Translate(moveDelta.x * Time.deltaTime * Speed, 0, 0);
+        }
     }
 }
