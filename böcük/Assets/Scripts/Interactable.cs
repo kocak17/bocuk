33ac7e609a6ff4c837df7547434d213aa938e72c
UgurONAL <onalug@itu.edu.tr>
2021-12-01 01:45:45 +0300

    Interaction system reworked.

---
+++ böcük/Assets/Scripts/Interactable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : Collidable
+{
+    public GameObject notifier;
+    public DialogueTrigger dialogueTrigger;
+
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.gameObject.layer == 9)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                dialogueTrigger.TriggerDialogue();
+            }
+        }
+    }
+
+    protected void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.layer == 9)
+        {
+            notifier.SetActive(true);
+        }
+    }
+
+    protected void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.layer == 9)
+        {
+            notifier.SetActive(false);
+        }
+    }
+}
