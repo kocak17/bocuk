3f69f712fa42aa50bf2457ef503aab3d9f109274
UgurONAL <onalug@itu.edu.tr>
2021-12-01 01:46:42 +0300

    Audio system implemented

---
+++ böcük/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSliderScript : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    void Start()
+    {
+        SoundManager.Instance.ChangeMasterVolume(slider.value);
+        slider.onValueChanged.AddListener(val => SoundManager.Instance.ChangeMasterVolume(val));
+    }
+}
