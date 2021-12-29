4d9c5adc222c6a010d67adec0ad08bd66c46efa4
DESKTOP-G10MKTB\Deniz <kocak17@itu.edu.tr>
2021-12-29 22:16:16 +0300

    yarım mountain

---
+++ böcük/Assets/Scripts/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+
+    
+{
+    public float targetclicks;
+    public Image Fill; 
+
+    float TotalClicks;
+
+    private void Start()
+    {
+        TotalClicks = 0;
+        Fill.fillAmount = 0;
+    }
+    public void AddClicks()
+    {
+        TotalClicks++;
+        Fill.fillAmount = TotalClicks / targetclicks;
+    }
+
+}
